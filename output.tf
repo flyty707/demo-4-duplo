4b9578d3b7289acd249c119354fa468aa73f839a
flyty707 <flyty707@gmail.com>
2024-01-31 13:19:18 -0800

    Update output.tf
    remarked LoadBalancer config for after cluster setup

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "cluster_name" {
   value = google_container_cluster.primary.name
 }
 
-output "webapp_service_ip" {
-  value = kubernetes_service.webapp.load_balancer_ingress[0].ip
-}
+# output "webapp_service_ip" {
+#   value = kubernetes_service.webapp.load_balancer_ingress[0].ip
+# }
